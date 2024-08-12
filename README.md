250e66c59037c0b1ae7a7b7370ad1d04107061a8
JINO-ROHIT <find.jinorohit@gmail.com>
2024-08-12 22:36:24 +0530

    finishing up

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# test_lambda_locally_sam
+# Testing AWS Lambda Locally with SAM 
+
+[![aws](https://cdn3.emoji.gg/emojis/8708-aws.png)](https://emoji.gg/emoji/8708-aws)
+
+This guide provides detailed steps on how to set up and test an AWS Lambda function locally using the AWS Serverless Application Model (SAM).
+
+
+
+1. **Initialize a SAM Project:**
+
+   Start by initializing a new SAM project. Run the following command and follow the prompts, including entering a project name (e.g., `sam-demo`).
+
+   ```bash
+   sam init
+
+2. **Edit the template.yaml File:**
+
+    Open the template.yaml file in your project directory. Modify it to define your Lambda function, including necessary configurations and resources.
+
+
+3. **Modify the event.json file.** 
+
+    This file should reflect the event data that your Lambda function will consume. Ensure the structure matches what your Lambda function expects.
+
+    ```bash
+    {
+    "key1": "value1",
+    "key2": "value2"
+    }
+    ```
+
+4. **Set Up Dependencies:**
+
+    Specify all the necessary dependencies in the requirements.txt file. This file should list all Python packages required by your Lambda function.
+
+5. **Build the Project:**
+
+    Build your SAM project to prepare it for local testing. This will compile your function and its dependencies into a .aws-sam directory.
+
+    ```bash
+    sam build
+    ```
+
+6. **Invoke the Lambda Function Locally:**
+
+    Finally, invoke your Lambda function locally to test it. Use the following command, replacing ChatFunction with your function's name and specifying the path to your event.json file.
+
+    ```bash
+    sam local invoke ChatFunction -e events/event.json
+    ```
+
+This command will simulate the Lambda execution environment on your local machine and process the event defined in event.json.
---
+++ sam-demo/sam_tutorial_demo/app.py
@@ -0,0 +1,71 @@
+import json
+import logging
+
+from llama_index.core.llms import ChatMessage
+from llama_index.llms.bedrock import Bedrock
+
+import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path='.sample.env')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def chat_assistant(user_query : str) -> str:
+
+    try:
+        messages = [
+            ChatMessage(
+                role = "system", 
+                content = "You are a chat assistant to help users with their queries"
+            ),
+            ChatMessage(role="user", content = user_query),
+        ]
+
+        llm = Bedrock(
+            model="amazon.titan-text-express-v1",
+            aws_access_key_id = os.getenv('aws_access_key_id'),
+            aws_secret_access_key = os.getenv('aws_secret_access_key'),
+            region_name = os.getenv('region_name')
+        )
+        resp = llm.chat(messages)
+
+        print(resp.message.content)
+        return resp.message.content
+    
+    except Exception as e:
+        logger.error(f"Unable to answer the query: {e}")
+        return ""
+
+
+def lambda_handler(event, context):
+    try:
+        event_body = json.loads(event['body'])
+        user_query = event_body.get('user_query')
+
+
+        if not user_query:
+            return {
+                'statusCode': 400,
+                'body': json.dumps('Invalid input: user_query is required')
+            }
+
+        response = chat_assistant(user_query)
+
+        if response:
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'message': response})
+            }
+        else:
+            return {
+                'statusCode': 500,
+                'body': json.dumps('Failed to generate a response')
+            }
+
+    except Exception as e:
+        logger.error(f"Exception in lambda_handler: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Internal server error: {e}')
+        }
